dc7f5df25eecb6fd3d1abe5a47f1b01551388974
Dickson <https://github.com/kobby-dicks/python_crash_course.git>
2024-03-29 12:35:04 +0000

    some changes took place

---
+++ python_crash_ccour.py
@@ -0,0 +1,39 @@
+# doing for loops
+names = [
+    "samuel oklu" , "prince asare" ,"jeremiaj adjetey"
+]
+for name in names:
+    print(f'How are you {name.title()}')
+    print("what are you doing today?\n")
+print("Alright good night buddies!!")
+
+# using range function under for loops
+for values in range(1,8):
+    print(values)
+
+# using range to write a list 
+scores = list(range(11,21))
+#print(scores)
+scores.append(21)
+print(scores)
+
+Data = []
+for value in range(2,11):
+    square = value**2 # squares of the numbers in data
+    Data.append(square)
+print(Data)
+#another way to write it is 
+Data = []
+for value in range(5,21):
+    Data.append(value**2)
+print(Data)
+
+# finding the min or max of numbers in a list
+#List Comprehensions. this helps to build 
+#in one single line of code
+Data = [value**2 for value in range(2,8)]
+print(Data)
+
+Data = [value for value in range(0,250)] #doing just a normal list
+print(Data)cle
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+Data = [value for value in range(0,250)] #doing just a normal list
+print(Data)
