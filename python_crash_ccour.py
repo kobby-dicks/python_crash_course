5f62d742135bfe7feaf1c5e45d2dcba896cbc663
Dickson <https://github.com/kobby-dicks/python_crash_course.git>
2024-03-30 18:44:30 +0000

    good

--- python_crash_ccour.py
+++ python_crash_ccour.py
@@ -145,3 +145,11 @@ else:
     metal_to_lift= 100
 print(f'You can try the {metal_to_lift}kg')
 
+# using if statements with lists
+requested_toppings = ['mashroom', 'meat', 'chicken']
+for requested_topping in requested_toppings:
+    if requested_topping == 'meat':
+         print(f"We are out of {requested_topping}.")
+    else:
+        print(f'{requested_topping} will be added')   
+print('Your oder will be ready in 5 minutes.')
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,10 +1,7 @@
-weight = 19
-if weight < 4:
-    metal_to_lift = 0
-elif weight <= 18:
-    metal_to_lift = 25
-elif weight <= 25:
-    metal_to_lift = 40
-else:
-    metal_to_lift= 100
-print(f'You can try the {metal_to_lift}kg')
+requested_toppings = ['mashroom', 'meat', 'chicken']
+for requested_topping in requested_toppings:
+    if requested_topping == 'meat':
+         print(f"We are out of {requested_topping}.")
+    else:
+        print(f'{requested_topping} will be added')   
+print('Your oder will be ready in 5 minutes.')
