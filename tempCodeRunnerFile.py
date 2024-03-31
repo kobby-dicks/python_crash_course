a23ab96b9e725930a54642ded81abee17960cfaf
Dickson <https://github.com/kobby-dicks/python_crash_course.git>
2024-03-31 21:16:20 +0000

    merge

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-Dimensions = (500,200)
-#print(Dimensions[0]) this could be done in another way using a loop
-#print(Dimensions[1])
-for Dimension in Dimensions:
-    print(f'the modified dimensions: {Dimension}')
