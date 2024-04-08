6a4aad7182e7f8a9448498ef4bd682c76eaa1e6c
Dickson <https://github.com/kobby-dicks/python_crash_course.git>
2024-04-08 00:00:52 +0000

    update

--- args_kwargs.py
+++ args_kwargs.py
@@ -2,4 +2,8 @@ def Number_of_fruits(*args, **kwargs,):
     print(args, type(args))
     print(kwargs, type(kwargs))
 
-    
+
+def add(x, *any_number):
+    for num in any_number:
+        x += num
+        print(x)
